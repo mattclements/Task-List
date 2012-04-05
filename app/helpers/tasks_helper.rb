3aea773ed7ea8e939019f112897bb520e8b75832
Matt Clements <matt.clements@debitfinance.co.uk>
2012-04-05 10:05:11 +0100

    Greyed out completed tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,6 +1,6 @@
 module TasksHelper
   def display_task_name(task)
-      style = 'text-decoration: line-through' if task.completed?
+      style = 'text-decoration: line-through; color: #AAA;' if task.completed?
       content_tag :h3, task.name, :style => "#{style}"
   end
 end
